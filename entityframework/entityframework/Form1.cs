154efa9644b92cbd4e2f3872d6ef1d3fa8e5fbfd
Alp <efe1998alp@hotmail.com>
2022-07-31 20:37:37 +0300

    windowsapplicationbasit

---
+++ entityframework/entityframework/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entityframework
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		ProductDal _productDal = new ProductDal();
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			LoadGrid();
+		}
+
+		public void LoadGrid()
+		{
+			dataGridView1.DataSource = _productDal.GetAll();
+		}
+
+		public void SearchProduct(string key)
+		{
+			dataGridView1.DataSource =_productDal.GetByName(key);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Product product = new Product
+			{
+				Name = textBox1.Text,
+				StockAmount = Convert.ToInt32(textBox2.Text),
+				UnitPrice = Convert.ToDecimal(textBox3.Text)
+			};
+
+			_productDal.Add(product);
+			LoadGrid();
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			_productDal.Delete(new Product { 
+				Id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value)				
+			});
+			LoadGrid();
+		}
+
+		private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+		{
+			textBox4.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+			textBox5.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+			textBox6.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			_productDal.Update(new Product
+			{
+				Id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value),
+				Name = textBox4.Text,
+				UnitPrice = Convert.ToDecimal(textBox5.Text),
+				StockAmount = Convert.ToInt32(textBox6.Text)
+			});
+
+			LoadGrid();
+		}
+
+		private void textBox7_TextChanged(object sender, EventArgs e)
+		{
+			SearchProduct(textBox7.Text);
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			dataGridView1.DataSource =_productDal.Order();
+		}
+	}
+}
---
+++ entityframework/entityframework/ProductDal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entityframework
+{
+	public class ProductDal
+	{
+		public List<Product> GetAll()
+		{
+			using(denemeContext denemeContext = new denemeContext())
+			{
+				return denemeContext.Products.ToList();
+			}
+		}
+
+		public List<Product> GetByName(string key)
+		{
+			using(denemeContext denemeContext = new denemeContext())
+			{
+				return denemeContext.Products.Where(p => p.Name.Contains(key)).ToList();
+			}
+		}
+
+		public List<Product> Order()
+		{
+			using (denemeContext denemeContext = new denemeContext())
+			{
+				return denemeContext.Products.OrderBy(p => p.Name).ToList();
+			}
+		}
+
+
+		public void Add(Product product)
+		{
+			using(denemeContext denemeContext = new denemeContext())
+			{
+				denemeContext.Products.Add(product);
+				denemeContext.SaveChanges();
+			}
+		}
+
+		public void Update(Product product)
+		{
+			using (denemeContext denemeContext = new denemeContext())
+			{
+				var entity = denemeContext.Entry(product);
+				entity.State = System.Data.Entity.EntityState.Modified;
+				denemeContext.SaveChanges();
+			}
+		}
+
+		public void Delete(Product product)
+		{
+			using (denemeContext denemeContext = new denemeContext())
+			{
+				var entity = denemeContext.Entry(product);
+				entity.State = System.Data.Entity.EntityState.Deleted;
+				denemeContext.SaveChanges();
+			}
+		}
+	}
+}
